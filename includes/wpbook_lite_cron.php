3b494a7f6a6b8eddee682313ae2893324e25ab71
John Eckman <eckman.john@gmail.com>
2012-09-29 19:44:53 +0000

    Catching undefined  variable
    git-svn-id: http://plugins.svn.wordpress.org/wpbook-lite/trunk@605884 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/wpbook_lite_cron.php
+++ includes/wpbook_lite_cron.php
@@ -107,7 +107,7 @@ function wpbook_lite_import_comments() {
   }  // end try-catch
 
   try {
-	$facebook->api('/me','GET');
+	$response = $facebook->api('/me','GET');
   } catch (FacebookApiException $e) {
 	if(WPBOOKDEBUG) {
 		$wpbook_message = 'Caught exception: ' .  $e->getMessage() .'Error code: '. $e->getCode();  
